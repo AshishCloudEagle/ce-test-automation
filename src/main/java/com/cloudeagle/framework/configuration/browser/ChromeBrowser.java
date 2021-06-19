22b797155dc97415017afffd90317f8304ea7e63
Krunal R <cp.krunal@gmail.com>
2021-06-19 18:13:11 +0530

    Headless & web driver manager added

--- src/main/java/com/cloudeagle/framework/configuration/browser/ChromeBrowser.java
+++ src/main/java/com/cloudeagle/framework/configuration/browser/ChromeBrowser.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import com.cloudeagle.framework.utility.DateTimeHelper;
 import com.cloudeagle.framework.utility.ResourceHelper;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * @author krana
@@ -24,6 +25,8 @@ public class ChromeBrowser {
 	public Capabilities getChromeCapabilities() {
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("start-maximized");
+		option.addArguments(new String[] { "headless" });
+		option.addArguments(new String[] { "window-size=1200x600" });
 		DesiredCapabilities chrome = DesiredCapabilities.chrome();
 		chrome.setJavascriptEnabled(true);
 		chrome.setCapability(ChromeOptions.CAPABILITY, option);
@@ -31,16 +34,14 @@ public class ChromeBrowser {
 	}
 
 	public WebDriver getChromeDriver(Capabilities cap) {
-		System.setProperty("webdriver.chrome.driver",
-				ResourceHelper.getResourcePath("driver/chromedriver.exe"));
-		System.setProperty("webdriver.chrome.logfile",
-				ResourceHelper.getResourcePath("logs/chromelogs/")
-						+ "chromelog" + DateTimeHelper.getCurrentDateTime()
-						+ ".log");
+//		System.setProperty("webdriver.chrome.driver", ResourceHelper.getResourcePath("driver/chromedriver.exe"));
+		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.logfile", ResourceHelper.getResourcePath("logs/chromelogs/") + "chromelog"
+				+ DateTimeHelper.getCurrentDateTime() + ".log");
 		return new ChromeDriver(cap);
 	}
-	
-	public WebDriver getChromeDriver(String hubUrl,Capabilities cap) throws MalformedURLException {
+
+	public WebDriver getChromeDriver(String hubUrl, Capabilities cap) throws MalformedURLException {
 		return new RemoteWebDriver(new URL(hubUrl), cap);
 	}
 
